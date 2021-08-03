a07e0b5cd40e603bb349a662d099cb91314142c9
Shashank_Chaudhary <55027786+shashank-crypto@users.noreply.github.com>
2021-08-03 23:13:33 +0530

    Search button working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import React , {useEffect , useState} from 'react'
+import Recipe from "./Recipe"
+
 
 function App() {
+
+  const APP_ID = "cf740888";
+  const APP_KEY = "f32945b38b8adbf02833051c74268666";
+
+  // const ExampleQuery = `https://api.edamam.com/api/recipes/v2?type=public&q=peanut&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  const [recipes , setRecipes] = useState([])
+  const [search , setSearch] = useState("");
+  const [query , setQuery] = useState('Peanut')
+
+  useEffect(() => {
+    getRecipes();
+  } , [query] )
+
+  // useEffect(fun , [])  ### When we want it to render only once (when the application mounts)
+
+  const getRecipes = () => {
+    fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`)
+    .then(data => data.json())
+    .then(response => {
+      setRecipes(response.hits)
+      console.log(response.hits)
+    })
+  }
+
+  const updateSearch = e => {
+    setSearch(e.target.value)
+    console.log(search)
+  }
+
+  const makeSearch = e => {
+    e.preventDefault()
+    setQuery(search)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>Recipe Search App</h1>
+      <form className="searchForm" onSubmit={makeSearch}>
+        <input type="text" className="searchBar" value={search} onChange={updateSearch}/>
+        <button  
+          type="submit" 
+          className="searchButton"
         >
-          Learn React
-        </a>
-      </header>
+          Search
+        </button>
+      </form>
+      {recipes.map((recipe,index) => (
+        <Recipe key={index} title={recipe.recipe.label} calories={recipe.recipe.calories} image={recipe.recipe.image}/>
+      ))}
     </div>
   );
 }
